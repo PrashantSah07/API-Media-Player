4ea85db6e67a257a562d5d47f8535cf08bac8ac2
prashant <prashantkumar855114@gmail.com>
2025-03-01 15:13:58 +0530

    some changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // This allows access from other devices
-    port: 5173, // Default Vite port
+    host: true,
+    port: 5173,
   },
 })
