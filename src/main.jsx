94533befdc59530bc54a3f1bd262056c52edca46
prashant <prashantkumar855114@gmail.com>
2025-03-01 13:42:53 +0530

    add a wildcard route

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './globle.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import './globle.css';
+import App from './App.jsx';
 import Calender from './Calender.jsx';
-import Images from './Images.jsx'
+import Images from './Images.jsx';
 import Movies from './Movies.jsx';
-import Songs from './Songs.jsx'
-import Error from './Error.jsx'
+import Songs from './Songs.jsx';
+import Error from './Error.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/calender" element={<Calender />} />
-      <Route path="/images" element={<Images />} />
-      <Route path="/movies" element={<Movies />} />
-      <Route path="/songs" element={<Songs />} />
-      <Route path="*" element={<Error />} /> 
-    </Routes>
-  </BrowserRouter>,
-)
+  <StrictMode>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/calender" element={<Calender />} />
+        <Route path="/images" element={<Images />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/songs" element={<Songs />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </HashRouter>
+  </StrictMode>
+);
